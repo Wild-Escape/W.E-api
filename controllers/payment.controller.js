69771a1a6984b6be2ada67c341c143b00677caa9
Vladys K <vldk0022@gmail.com>
2025-03-01 10:39:05 +0100

    feat: modifiesd temporaly payment model

--- controllers/payment.controller.js
+++ controllers/payment.controller.js
@@ -1,19 +1,16 @@
 const Payment = require("../models/Payment.model");
 
 module.exports.createPayment = async (req, res, next) => {
-    const { user, experience, price, amount, dates } = req.body;
-    try {
-        const newPayment = new Payment({
-        user: req.currentUserId,
-        experience,
-        price,
-        amount,
-        dates,
-        });
-        const savedPayment = await newPayment.save();
-        res.status(201).json(savedPayment);
-    } catch (error) {
-        next(error);
-    }
-
-}
+  const { experience, price } = req.body;
+  try {
+    const newPayment = new Payment({
+      user: req.currentUserId,
+      experience,
+      price,
+    });
+    const savedPayment = await newPayment.save();
+    res.status(201).json(savedPayment);
+  } catch (error) {
+    next(error);
+  }
+};
--- models/Payment.model.js
+++ models/Payment.model.js
@@ -4,10 +4,10 @@ const PaymentSchema = new mongoose.Schema(
   {
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     experience: { type: mongoose.Schema.Types.ObjectId, ref: "Experience", required: true },
-    dates: { 
-      start: { type: Date, required: true },
-      end: { type: Date, required: true }
-     },
+    // dates: { 
+    //   start: { type: Date, required: true },
+    //   end: { type: Date, required: true }
+    //  },
     price: { 
       amount: { type: Number, required: true },
       currency: { type: String, required: true }
