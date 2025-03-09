9a2c398aa9b5ff544198a3191484123c9745fa4f
Vladys K <vldk0022@gmail.com>
2025-03-09 20:33:07 +0100

    updated bcrypt js

--- models/User.model.js
+++ models/User.model.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 const EMAIL_PATTERN =
   /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
